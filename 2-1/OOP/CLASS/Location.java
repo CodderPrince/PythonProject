05f969d9cde123690858ce99c4d6c5797135e65b
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:18:50 +0600

    Create Location.java

---
+++ 2-1/OOP/CLASS/Location.java
@@ -0,0 +1,61 @@
+/*
+ * Problem: 9.9
+ * Check largest data in a two dimensional Array
+ */
+import java.util.Scanner;
+
+class Location {
+    //create public data field row, column and maxValue
+    public int row;
+    public int column;
+    public double maxValue;
+
+    public Location(int row, int column, double maxValue) {
+        this.row = row;
+        this.column = column;
+        this.maxValue = maxValue;
+    }
+}
+
+class Main {
+    public static Location locateLargest(double[][] a) {
+        int maxRow = 0;
+        int maxColumn = 0;
+        double maxVal = a[0][0];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] > maxVal) {
+                    maxVal = a[i][j];
+                    maxRow = i;
+                    maxColumn = j;
+                }
+            }
+        }
+        
+        return new Location(maxRow, maxColumn, maxVal);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows and columns in the array: ");
+        int rows = input.nextInt();
+        int columns = input.nextInt();
+
+        double[][] array = new double[rows][columns];
+        System.out.println("Enter the array:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[i][j] = input.nextDouble();
+            }
+        }
+
+        Location loc = locateLargest(array);
+        System.out.println("The location of the largest element is " + loc.maxValue +
+                           " at (" + loc.row + ", " + loc.column + ")");
+        
+        input.close();
+    }
+}
