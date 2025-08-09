2b273f4d9fbcc09800f1018a15ba5a321de0c2e8
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:12:53 +0600

    Create a.cpp

---
+++ 13 CONTEST 22 MARCH, 2023/cpclub/JAN 12, 24 FIRST/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main()
+{
+    ll t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cout << "I love CP CLUB BRUR\n";
+    }
+
+    return 0;
+}
