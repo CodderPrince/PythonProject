c3f31cfb6bd74af2edf06e654cf55c0346d42c13
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:19:37 +0600

    Create SecondClass.java

---
+++ 2-1/OOP/JAVA/APNA COLLEGE/a.java/SecondClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Hello Prince");
+        System.out.println("Hello Prince");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        System.out.print("*\n**");
+    }
+
+}
