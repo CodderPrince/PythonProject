ed68ecb9106c33396e9a919c90579ee53916a792
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:13:22 +0600

    Create jan5.cpp

---
+++ 2-1/DSA/CLASS/.cph/jan5.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        
+    }
+
+    return 0;
+}
