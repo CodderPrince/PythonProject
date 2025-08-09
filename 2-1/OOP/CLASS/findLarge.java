a23848cb6951b0d0d38b00ff7c4da58705a13593
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:18:48 +0600

    Create findLarge.java

---
+++ 2-1/OOP/CLASS/findLarge.java
@@ -0,0 +1,11 @@
+public class findLarge {
+
+    public static void main(String[] args){
+        //create scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter");
+        so
+        
+    }
+}
