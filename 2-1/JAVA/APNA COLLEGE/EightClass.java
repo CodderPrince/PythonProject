2e5bd49a597aa5c6f417711b9393a9cebf218591
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:17:39 +0600

    Create EightClass.java

---
+++ 2-1/JAVA/APNA COLLEGE/EightClass.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class EightClass {
+    // public static void main(String[] args) {
+
+    // public static void average(){
+    // Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter first number: ");
+    // int a = sc.nextInt();
+    // System.out.print("Enter Second number: ");
+    // int b = sc.nextInt();
+    // System.out.print("Enter third number: ");
+    // int c = sc.nextInt();
+
+    // float avg = (a+b+c)/3.0f;
+    // System.out.printf("Average: %.2f\n",avg);
+    // }
+    // }
+
+    // public static void sumOfAllOddFunction() {
+    // Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter a number: ");
+    // int n = sc.nextInt();
+
+    // int sum = 0;
+    // for (int i = 1; i <= n; i++) {
+    // if (i % 2 != 0) {
+    // sum += i;
+    // }
+    // }
+    // System.out.println("Sum of all odd numbers: " + sum);
+    // }
+
+    // p3
+    /*
+     * public static void MaxAndMin() {
+     * Scanner sc = new Scanner(System.in);
+     * System.out.print("Enter number 1: ");
+     * int a = sc.nextInt();
+     * System.out.print("Enter number 2: ");
+     * int b = sc.nextInt();
+     * System.out.print("Enter number 3: ");
+     * int c = sc.nextInt();
+     * 
+     * // method 1 if else
+     * if (a > b) {
+     * System.out.println("Max is: " + a);
+     * System.out.println("Min is: " + b);
+     * } else {
+     * System.out.println("Max is: " + b);
+     * System.out.println("Min is: " + a);
+     * }
+     * 
+     * // method 2 using build in function
+     * int Max = Math.max(a, Math.max(b, c));
+     * int Min = Math.min(a, b);
+     * System.out.println("Build in Max is: " + Max);
+     * System.out.println("Build in Min is: " + Min);
+     * }
+     */
+
+    public static void main(String[] args) {
+
+        // p1
+        // average();
+
+        // p2
+        // sumOfAllOddFunction();
+
+        // p3
+        // max annd min
+        // MaxAndMin();
+
+        // p4
+        // Circumference
+        // not more enough
+    }
+
+}
