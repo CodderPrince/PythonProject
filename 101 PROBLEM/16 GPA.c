79ef2f7a80c57179b1f001f4b8a64e0648dbec32
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:12:11 +0600

    Create 16 GPA.c

---
+++ 101 PROBLEM/16 GPA.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+double gpa;
+printf("Enter your gpa: ");
+scanf("%lf",&gpa);
+if(gpa==5.00)
+{
+    printf("YES\n");
+}
+else
+{
+    printf("NO\n");
+}
+
+return 0;
+}
