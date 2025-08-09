7e65016b4d82dbbf311cbf5e84033fbe53f9ae4a
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:12:42 +0600

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007            // 1e9 + 7
+#define PI 3.14159265358979323846 // acos(-1)
+#define nl "\n"
+
+int main()
+{
+    int t;
+    cout << "Enter a number: ";
+    cin >> t;
+    while (t--)
+    {
+        cout << "Prince\n";
+    }
+
+    return 0;
+}
