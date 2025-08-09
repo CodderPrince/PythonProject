e902baaf062a2360578853aa214866bd9ac8dcad
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-09 19:18:07 +0600

    Create tempCodeRunnerFile.java

---
+++ 2-1/JAVA/APNA COLLEGE/tempCodeRunnerFile.java
@@ -0,0 +1,68 @@
+
+public class preOrderTravel {
+
+    // Node class to represent a tree node
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        // Constructor to create a new node
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // BinaryTree class
+    static class BinaryTree {
+
+        static int ind = -1;
+
+        // Function to build the binary tree
+        public static Node buildTree(int nodes[]) {
+
+            ind++; // Move to the next index
+
+            // Base case - if the node is -1, it means null
+            if (nodes[ind] == -1) {
+                return null;
+            }
+
+            // Create a new node with the current value from the array
+            Node newNode = new Node(nodes[ind]);
+
+            // Recursively build the left and right subtrees
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode; // Return the constructed node
+        }
+
+        public static void preOrder(Node root) {
+            //base case
+            if (root == null) {
+                return;
+            }
+
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Array representation of the tree
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        // Create an instance of BinaryTree
+        BinaryTree tree = new BinaryTree();
+
+        // Build the binary tree from the array and get the root node
+        Node root = tree.buildTree(nodes);
+        preOrder(root);
+    }
+
+}
