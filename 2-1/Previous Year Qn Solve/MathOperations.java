80a90972b2bd65b46167fa43ce5a3d4d53202169
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:45:43 +0600

    Create MathOperations.java

---
+++ 2-1/Previous Year Qn Solve/MathOperations.java
@@ -0,0 +1,15 @@
+// MathOperations.java
+public class MathOperations {
+    // Static final variable (constant)
+    public static final double PI = 3.14159;
+
+    // Static method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Static method to multiply two numbers
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
