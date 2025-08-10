8e3c243820278fdfc3a74113f2d1c12c188341c1
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:43:39 +0600

    Create a.java

---
+++ 2-1/OOP/OOP/Practice/a.java
@@ -0,0 +1,8 @@
+public class a {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("n");
+        System.out.println();
+    }
+    
+}
