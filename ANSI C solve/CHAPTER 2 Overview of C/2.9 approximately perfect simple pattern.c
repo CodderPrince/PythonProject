4095175d4be96dbe41e14ccb08c398fea25c510d
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:48:57 +0600

    Create 2.9 approximately perfect simple pattern.c

---
+++ ANSI C solve/CHAPTER 2 Overview of C/2.9 approximately perfect simple pattern.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+printf("------------");printf("                "); printf(" -------------\n");
+printf("|           |");printf("                ");printf("|           |\n");
+printf("|           |");printf("                ");printf("|           |\n");
+printf("|           |");printf(">>------------->");printf("|           |\n");
+printf("|           |");printf("                ");printf("|           |\n");
+printf("------------");printf("                "); printf(" -------------");
+return 0;
+}
