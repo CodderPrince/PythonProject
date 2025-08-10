300f0ae3465f0741259e803580a166924d587e68
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:45:42 +0600

    Create Main.java

---
+++ 2-1/Previous Year Qn Solve/Main.java
@@ -0,0 +1,38 @@
+class MyRunnable implements Runnable {
+    private String threadName;
+
+    public MyRunnable(String threadName) {
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(threadName + " - Count: " + i);
+            try {
+                Thread.sleep(1000); // Simulate some work with a sleep
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
+        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Both threads have finished execution");
+    }
+}
