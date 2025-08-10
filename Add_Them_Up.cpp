d815e4c2e113885f7da767c3ebbc5ac7ccbee65c
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:46:48 +0600

    Create Add_Them_Up.cpp

---
+++ Add_Them_Up.cpp
@@ -0,0 +1,22 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcpp"
+#include <bits/stdc++.h>
+#pragma GCC diagnostic pop
+
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        
+    }
+
+    return 0;
+}
