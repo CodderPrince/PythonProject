644c86e190bce31a03b47879374782b5dd2fb3c8
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:43:38 +0600

    Create a.java

---
+++ 2-1/OOP/OOP/a.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class a{
+    public static void main(String[] args) {
+        System.out.println("Hello Prince!");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
