ceded66d9ac7186f95727662bd5fc0942f74f548
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:48:42 +0600

    Create 2.13.0.c

---
+++ ANSI C solve/CHAPTER 2 Overview of C/2.13.0.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+double x1,y1,x2,y2,D;
+printf("Enter the value of four variables:\n\t");
+scanf("%lf%lf%lf%lf", &x1,&y1,&x2,&y2);
+D=sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+printf("\tD=\t%.2lf",D);
+
+return 0;
+}
