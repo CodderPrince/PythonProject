b9f548f99cbba7db6ef841f9709797c47e869f71
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:46:04 +0600

    Create tempCodeRunnerFile.java

---
+++ 2-1/Previous Year Qn Solve/tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+public class a {
+
+    public static void main(String[] args) {
+        int x = 80000000;
+
+        while (x > 0) {
+            x++;
+        }
+
+        System.out.println("x is " + x);
+    }
+}
