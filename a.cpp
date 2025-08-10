ab6a6a69117558794bd6b583e142455b49a26a41
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-10 15:46:43 +0600

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        cout<<"Prince";
+    }
+
+    return 0;
+}
