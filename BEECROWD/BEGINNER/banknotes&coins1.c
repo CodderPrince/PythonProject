5e0fbdce1c3f11035c27c0ae98e3cdd82a751e93
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-11 08:00:58 +0600

    Create banknotes&coins1.c

---
+++ BEECROWD/BEGINNER/banknotes&coins1.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+ #include<math.h>
+ int main(){
+   //100,50,20,10,5,2,1,.5,.25,.01
+   //10000,5000,2000,1000,500,200,100,50,25,1
+   double t;
+   scanf("%lf",&t);
+   int a,b,c,d,e,f,g,h,i,j,k,l;
+   l= t*100;
+   a= l/10000;
+   t= l%10000;
+   b= 
+ }
