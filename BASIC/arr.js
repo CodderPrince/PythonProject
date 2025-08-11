0cff78f34bedc37cb709f5fe50ef5e83a19d63f3
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-11 08:00:14 +0600

    Create arr.js

---
+++ BASIC/arr.js
@@ -0,0 +1,12 @@
+let fruits = ["Apple", "Banana", "Orange"];
+
+// Adding an item to the array
+fruits.push("Grapes");
+
+// Removing the last item from the array
+fruits.pop();
+
+// Iterating over the array
+fruits.forEach(function(fruit) {
+    console.log(fruit);
+});
