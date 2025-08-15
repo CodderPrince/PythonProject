f6d2a87d5ee65d709b0da2ef3d5e6172be963ea2
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-15 06:38:14 +0600

    Create ho.c

---
+++ BEECROWD/BEGINNER/ho.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+    for(i=1; i<num; i++)
+    {
+        printf("Ho ");
+       //input number theke 1 bar kom print korbo and last e arekta print kore dibo 
+    }
+    printf("Ho!\n");
+    return 0;
+}
