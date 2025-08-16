031ac178cdc2a676db59cfcc91409dffac51438d
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-16 18:27:08 +0600

    Create salaray.c

---
+++ BEECROWD/BEGINNER/salaray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a,b;
+    float c,d;
+    scanf("%d %d %f",&a,&b,&d);
+    c= b*d;
+    printf("NUMBER = %d\n",a);
+    printf("SALARY = U$ %.2f\n",c);
+    
+    return 0;
+}
