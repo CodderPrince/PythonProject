9e8b87197027f005f466c15ce99994f53b6250ea
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-23 16:43:06 +0600

    Create 3.8 square of a number.c

---
+++ CHAPTER 3 Constants, Variables and Data Types/3.8 square of a number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num,s;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    s= num*num;
+    printf("Square of %d number is:%d\n",num,s);
+
+    return 0;
+}
