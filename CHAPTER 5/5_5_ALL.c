ba70f1241c71d67a943df9173391fb43291e76ed
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-24 19:09:46 +0600

    Create 5_5_ALL.c

---
+++ CHAPTER 5/5_5_ALL.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int mat,phy,che,t1,t2;
+    scanf("%d %d %d",&mat,&phy,&che);
+    if(mat>=60 && phy>=50 && che>=40 && t1>=200)
+    {
+        printf("The student is eligible for admission.\n ");
+    }
+    else if(t2>=150)
+    {
+        printf("The student is eligible for admission.\n ");
+    }
+    else{
+        printf("The student is not eligible for admission.\n ");
+    }
+}
