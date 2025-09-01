a507fabd9ec5513a1cbb43fbf9caba9579e807b7
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-02 00:53:30 +0600

    Create a.cpp

---
+++ CODEFORCES/DIV 2 NOV 3, 2023/a.cpp
@@ -0,0 +1,20 @@
+#include<bits\stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll arr[2*n];
+        ll i;
+        for(i=0; i<2*n; i++)
+        {
+            cin>>arr[i];
+        }
+        for(i=0; i<a)
+    }
+}
