d1f830e2dc823653ccc15dce2ad6b900203dcfb4
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-14 00:23:01 +0600

    Create cc.c

---
+++ CODEFORCES/DIV 4 SEP 21, 2023/cc.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int BlackCells(char s[], int n, int k) 
+{
+    int operations = 0;
+    
+    for (int i = 0; i < n; ) 
+    {
+        int j = i;
+        
+        while (j < n && s[j] != 'B') 
+        {
+            j++;
+        }
+        
+        if (j == n) 
+        {
+            break;
+        }
+        
+        for (int x = j; x < j + k && x < n; x++) 
+        {
+            s[x] = 'W';
+        }
+        
+        operations++;
+    }
+    
+    return operations;
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    
+    while (t--) {
+        int n, k;
+        scanf("%d %d", &n, &k);
+        
+        char s[n + 1]; 
+        scanf("%s", s);
+        
+        int operations = BlackCells(s, n, k);
+        printf("%d\n", operations);
+    }
+    
+    return 0;
+}
