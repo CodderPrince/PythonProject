b2035f95d2c0b2b8fc19e25ff8947713a5e8b2b2
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-20 07:48:31 +0600

    Create A_Buy_a_Shovel.cpp

---
+++ CODEFORCES/PRACTICE/A_Buy_a_Shovel.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main()
+{
+   for(int i=0; i<5; i++)
+   {
+    int x;
+    cout<<i<<" ";
+   }
+
+return 0;
+}
