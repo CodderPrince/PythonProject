e43653666c47bb275e9e551f5e44dee414ce0535
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-21 00:30:18 +0600

    Create again_twenty_five.c

---
+++ CODEFORCES/PRACTICE/again_twenty_five.c
@@ -0,0 +1,15 @@
+//again_twenty_five.c
+//prince
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    long long int n,x;
+    scanf("%lld",&n);
+    //x = pow(5,n);
+    printf("25\n");
+
+    return 0;
+}
