37ce483d84da5acabc7df1289b88bc24ea779dd1
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-20 07:48:25 +0600

    Create 1ton.cpp

---
+++ CODEFORCES/PRACTICE/1ton.cpp
@@ -0,0 +1,17 @@
+// prince
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
