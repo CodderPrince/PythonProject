6bc7344827b36fef9bede69c7d82fb554a870bf0
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-21 00:30:22 +0600

    Create ahhf.c

---
+++ CODEFORCES/PRACTICE/ahhf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+    int main()
+    {
+         int a, b, c, tmp;
+         scanf("%d %d %d", &a, &b, &c);
+         int *x,*y,*z;
+         *x=a;
+         *y=b;
+         *z=c;
+         if(*x>*y && *x>*z)
+         {
+            int temp=*x;
+         }
+         else if("")
+        printf("%d eh o maior\n", tmp);
+        return 0;
+    }    
