2f27af1fa1cd22d7713e3e044c5cf8c57ccf476c
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-22 09:41:33 +0600

    Create B_Normal_Problem.cpp

---
+++ CODEFORCES/PRACTICE/B_Normal_Problem.cpp
@@ -0,0 +1,26 @@
+//prince
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string a;
+        cin >> a;
+        reverse(a.begin(), a.end());
+        for (char& c : a) {
+            if (c == 'p') {
+                c = 'q';
+            } else if (c == 'q') {
+                c = 'p';
+            }
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
