3e97448a239edbc6df9cd752bb9196516e78747e
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-22 09:41:36 +0600

    Create B_Startup.cpp

---
+++ CODEFORCES/PRACTICE/B_Startup.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>  // You can keep this for convenience
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);  // Disable synchronization with C streams
+    cin.tie(nullptr);  // Untie cin from cout to improve I/O performance
+
+    // Direct output without flushing
+    cout << "GCC setup is successful!" << "\n";
+    
+    return 0;
+}
