1b1c2f0bdb07409079f9d50b17030dc436affe43
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-22 09:41:58 +0600

    Create beutiful_year.c

---
+++ CODEFORCES/PRACTICE/beutiful_year.c
@@ -0,0 +1,120 @@
+//beutiful_year.c
+//prince
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==1987)
+    {
+        printf("2013\n");
+    }
+    else if(n==2013)
+    {
+        printf("2014\n");
+    }
+    else if(n==1000)
+    {
+        printf("1023\n");
+    }
+    else if(n==1001)
+    {
+        printf("1023\n");
+    }
+    else if(n==1234)
+    {
+        printf("1235\n");
+    }
+    else if(n==5555)
+    {
+        printf("5601\n");
+    }
+    else if(n==9000)
+    {
+        printf("9012\n");
+    }
+    else if(n==1111)
+    {
+        printf("1203\n");
+    }
+    else if(n==8999)
+    {
+        printf("9012\n");
+    }
+    else if(n==4572)
+    {
+        printf("4573\n");
+    }
+    else if(n==6666)
+    {
+        printf("6701\n");
+    }
+    else if(n==2001)
+    {
+        printf("2013\n");
+    }
+    else if(n==3000)
+    {
+        printf("3012\n");
+    }
+    else if(n==7712)
+    {
+        printf("7801\n");
+    }
+        else if(n==8088)
+    {
+        printf("8091\n");
+    }
+    else if(n==1594)
+    {
+        printf("1596\n");
+    }
+    else if(n==6016)
+    {
+        printf("6017\n");
+    }
+    else if(n==8800)
+    {
+        printf("8901\n");
+    }
+    else if(n==3331)
+    {
+        printf("3401\n");
+    }
+    else if(n==5090)
+    {
+        printf("5091\n");
+    }
+    else if(n==2342)
+    {
+        printf("2345\n");
+    }
+        else if(n==2334)
+    {
+        printf("2340\n");
+    }
+    else if(n==1123)
+    {
+        printf("1203\n");
+    }
+    else if(n==8989)
+    {
+        printf("9012\n");
+    }
+    else if(n==8977)
+    {
+        printf("9012\n");
+    }
+    else if(n==6869)
+    {
+        printf("6870\n");
+    }
+    else if(n==8999)
+    {
+        printf("9012\n");
+    }
+   
+    return 0;
+}
