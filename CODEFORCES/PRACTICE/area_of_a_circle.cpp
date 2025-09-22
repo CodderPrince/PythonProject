c20ba079b702b73d953f3831164bda3ebbcf3ec7
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-22 09:41:21 +0600

    Create area_of_a_circle.cpp

---
+++ CODEFORCES/PRACTICE/area_of_a_circle.cpp
@@ -0,0 +1,18 @@
+//area_of_a_circle.c
+//prince
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double R;
+    cin >> R;
+
+    double pi = 3.141592653;
+    double area = pi * R * R;
+
+    cout << fixed << setprecision(9) << area << endl;
+
+    return 0;
+}
