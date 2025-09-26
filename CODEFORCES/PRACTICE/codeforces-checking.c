a9210f1f3db23e17a68272f46244ffc7a2e19e33
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-26 08:37:09 +0600

    Create codeforces-checking.c

---
+++ CODEFORCES/PRACTICE/codeforces-checking.c
@@ -0,0 +1,25 @@
+// prince
+
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        char ch;
+        scanf(" %c", &ch);
+
+        if (ch == 'c' || ch == 'o' || ch == 'd' || ch == 'e' || ch == 'f' || ch == 'r' || ch == 's')
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
