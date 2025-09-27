3871311a316c7c42e7c5647fb23b4ffc581bb251
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-27 09:03:25 +0600

    Create digits_summation.c

---
+++ CODEFORCES/PRACTICE/digits_summation.c
@@ -0,0 +1,19 @@
+//digits_summation.c
+//prince
+
+#include <stdio.h>
+
+int main() 
+{
+    long long N, M;
+    scanf("%lld %lld", &N, &M);
+
+   
+    int lastDigitN = N % 10;
+    int lastDigitM = M % 10;
+    int sum = lastDigitN + lastDigitM;
+
+    printf("%d\n", sum);
+
+    return 0;
+}
