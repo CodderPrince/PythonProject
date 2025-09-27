26bd0e1b9c3221f7241a5d8e48e5e52fa669eb4d
Md. An Nahian Prince <annahian44@gmail.com>
2025-09-27 09:03:21 +0600

    Create difference.c

---
+++ CODEFORCES/PRACTICE/difference.c
@@ -0,0 +1,14 @@
+//difference.c
+//prince
+
+#include<stdio.h>
+
+int main()
+{
+    long long int a,b,c,d,x;
+    scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+    x = (a*b)-(c*d);
+    printf("Difference = %lld\n",x);
+
+    return 0;
+}
