8bb29e9d9263121b688d24e6fed0f885780d3565
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-06 00:35:08 +0600

    Create hulk1.c

---
+++ CODEFORCES/PRACTICE/hulk1.c
@@ -0,0 +1,27 @@
+//hulk1.c
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i;
+    for(i=1; i<=n; i++)
+    {
+        if(i%2==0)
+        {
+            printf("I love ");
+        }
+        else
+        {
+            printf("I hate ");
+        }
+        if(i!=n)
+        {
+            printf("that ");
+        }
+    }
+    printf("it ");
+
+    return 0;
+}
