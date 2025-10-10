cc3bdac66b63843a856fe309989c7e28643e2e40
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-10 09:23:21 +0600

    Create max-spilit.cpp

---
+++ CODEFORCES/PRACTICE/max-spilit.cpp
@@ -0,0 +1,92 @@
+/**************************************************
+* Name:  MD. AN NAHIAN PRINCE                    *
+* Codeforces: Hacker_3.0                         *
+* University: BEGUM ROKEYA UNIVERSITY, RANGPUR   *
+* Department: COMPUTER SCIENCE AND ENGINEERING   *
+* Email: annahian44@gmail.com                    *
+**************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int i = (a); i > (b); i--)
+#define F0R(i, n) FOR(i, 0, n)
+#define ROF0R(i, n) ROF(i, n, 0)
+#define EACH(x, a) for (auto &x : a)
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define ld long double
+
+#define mod 1000000007            // 1e9 + 7
+#define PI 3.14159265358979323846 // acos(-1)
+#define endl "\n"
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Additional Macros and Constants
+#define sz(v) (int)v.size()
+#define INF 1000000000
+
+// Additional Data Structures
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+typedef map<int, int> mi;
+typedef unordered_map<int, int> umi;
+
+/****CREATE NEW FUNCTION HERE****/
+
+
+/****MAIN FUNCTION IS HERE****/
+
+int main()
+{
+    char str[10001];
+    gets(str);
+    ll i, Lcount=0,Rcount=0,count=0;
+    for(i=0;i<strlen(str); i++)
+    {
+        
+        if(str[i]=='L' )
+        {
+            Lcount++;
+        }
+        else if(str[i]=='R')
+        {
+            Rcount++;
+        }
+        if(Lcount == Rcount)
+        {
+            count = Lcount+Rcount;
+            //cout<<count<<endl;
+            for(i=0; i<count/2; i++)
+            {
+                cout<<"L";
+            }
+            for(i=0; i<count/2; i++)
+            {
+                cout<<"R";
+            }
+            
+            Lcount=0;
+            Rcount=0;
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
