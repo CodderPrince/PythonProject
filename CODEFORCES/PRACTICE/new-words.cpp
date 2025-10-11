07cb220776710e3fd0946310b861107890efe482
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-11 08:50:59 +0600

    Create new-words.cpp

---
+++ CODEFORCES/PRACTICE/new-words.cpp
@@ -0,0 +1,98 @@
+/**************************************************
+ * Name:  MD. AN NAHIAN PRINCE                    *
+ * Codeforces: Hacker_3.0                         *
+ * University: BEGUM ROKEYA UNIVERSITY, RANGPUR   *
+ * Department: COMPUTER SCIENCE AND ENGINEERING   *
+ * Email: annahian44@gmail.com                    *
+ **************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int i = (a); i > (b); i--)
+#define F0R(i, n) FOR(i, 0, n)
+#define ROF0R(i, n) ROF(i, n, 0)
+#define EACH(x, a) for (auto &x : a)
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define ld long double
+
+#define mod 1000000007            // 1e9 + 7
+#define PI 3.14159265358979323846 // acos(-1)
+#define endl "\n"
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Additional Macros and Constants
+#define sz(v) (int)v.size()
+#define INF 1000000000
+
+// Additional Data Structures
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+typedef map<int, int> mi;
+typedef unordered_map<int, int> umi;
+
+/****CREATE NEW FUNCTION HERE****/
+int countEgypt(string s)
+{
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+    int countE = 0, countG = 0, countY = 0, countP = 0, countT = 0;
+
+    for (char ch : s)
+    {
+        if (ch == 'e')
+        {
+            countE++;
+        }
+        else if (ch == 'g')
+        {
+            countG++;
+        }
+        else if (ch == 'y')
+        {
+            countY++;
+        }
+        else if (ch == 'p')
+        {
+            countP++;
+        }
+        else if (ch == 't')
+        {
+            countT++;
+        }
+    }
+
+    int countEgypt = min({countE, countG, countY, countP, countT});
+    return countEgypt;
+}
+
+/****MAIN FUNCTION IS HERE****/
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int count = countEgypt(s);
+
+    cout << count << endl;
+
+    return 0;
+}
