70af99bd0c1e83f4822caf13f09cf5fbf7299a4c
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-11 08:51:07 +0600

    Create numbers-of-histogram.cpp

---
+++ CODEFORCES/PRACTICE/numbers-of-histogram.cpp
@@ -0,0 +1,24 @@
+// prince
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+    int n;
+    cin >> n;
+    int i, j, x;
+    for (i = 0; i < n; i++)
+    {
+        cin >> x;
+        for (j = 0; j < x; j++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
