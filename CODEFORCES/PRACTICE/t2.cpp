fd42dadd18d9990d2817ca847b25fc54bb830c46
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-18 00:58:26 +0600

    Create t2.cpp

---
+++ CODEFORCES/PRACTICE/t2.cpp
@@ -0,0 +1,52 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int i = (a); i > (b); i--)
+#define F0R(i, n) FOR(i, 0, n)
+#define ROF0R(i, n) ROF(i, n, 0)
+#define EACH(x, a) for (auto &x : a)
+
+#define fastio           \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);          \
+  cout.tie(NULL)
+#define ll long long
+#define ld long double
+
+#define mod 1000000007      // 1e9 + 7
+#define PI 3.14159265358979323846 // acos(-1)
+#define endl "\n"
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Additional Macros and Constants
+#define sz(v) (int)v.size()
+#define INF 1000000000
+
+// Additional Data Structures
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+typedef map<int, int> mi;
+typedef unordered_map<int, int> umi;
+
+int main() {
+  // Your code starts here
+  int t;
+  cin >> t;
+  cout<<"Hello "<<t;
+  return 0;
+}
