f30a608ca9a2704f67ee4a2754889e0e7233abff
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-20 08:48:31 +0600

    Create welcome_for.c

---
+++ CODEFORCES/PRACTICE/welcome_for.c
@@ -0,0 +1,20 @@
+//Welcome for you with Conditions
+//prince
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>=b)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+
+    return 0;
+}
