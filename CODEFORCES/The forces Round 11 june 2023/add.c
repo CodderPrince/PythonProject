b3379d27a72c5407c3e37f055c0184648721e482
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-28 07:41:01 +0600

    Create add.c

---
+++ CODEFORCES/The forces Round 11 june 2023/add.c
@@ -0,0 +1,16 @@
+// prince
+
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", add(a, b));
+
+    return 0;
+}
