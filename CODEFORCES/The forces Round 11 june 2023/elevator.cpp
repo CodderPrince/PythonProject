775d4633de8f7fd24f872cf0fdebc048a21664fd
Md. An Nahian Prince <annahian44@gmail.com>
2025-10-29 07:40:07 +0600

    Create elevator.cpp

---
+++ CODEFORCES/The forces Round 11 june 2023/elevator.cpp
@@ -0,0 +1,96 @@
+/**************************************************
+ *..........Bismillahir Rahmanir Raheem...........*
+ *------------------------------------------------*
+ * Name:  MD. AN NAHIAN PRINCE                    *
+ * Codeforces: Hacker_3.0                         *
+ * University: BEGUM ROKEYA UNIVERSITY, RANGPUR   *
+ * Department: COMPUTER SCIENCE AND ENGINEERING   *
+ * Email: annahian44@gmail.com                    *
+ *------------------------------------------------*
+ **************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int i = (a); i > (b); i--)
+#define F0R(i, n) FOR(i, 0, n)
+#define ROF0R(i, n) ROF(i, n, 0)
+#define EACH(x, a) for (auto &x : a)
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define ld long double
+
+#define mod 1000000007            // 1e9 + 7
+#define PI 3.14159265358979323846 // acos(-1)
+#define endl "\n"
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Additional Macros and Constants
+#define sz(v) (int)v.size()
+#define INF 1000000000
+
+// Additional Data Structures
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+typedef map<int, int> mi;
+typedef unordered_map<int, int> umi;
+
+/****CREATE NEW FUNCTION HERE****/
+
+/****MAIN FUNCTION IS HERE****/
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    // char str[101];
+    string str;
+
+    // gets(str);
+    cin >> str;
+    ll t;
+    cin >> t;
+    if ((str == "front" && t == 1) || (str == "back" && t == 2))
+    {
+        cout << "L\n";
+    }
+    else
+    {
+        cout << "R\n";
+    }
+    /*if (strcmp(str, "front") == 0 && t == 1)
+    {
+        cout << "L\n";
+    }
+    else if (strcmp(str, "front") == 0 && t == 2)
+    {
+        cout << "R\n";
+    }
+    else if (strcmp(str, "back") == 0 && t == 1)
+    {
+        cout << "L\n";
+    }
+    else if (strcmp(str, "back") == 0 && t == 2)
+    {
+        cout << "R\n";
+    }*/
+
+    return 0;
+}
