9f1b79eacc733d4a5f0c0e482ef21bec73ad7b21
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-04 07:47:55 +0600

    Create 17.py

---
+++ CodeMama/17.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Write Python code from here
+n=int(input())
+if n>0:
+    print(f"{n} is a positive number.")
+elif n==0:
+    print(f"The number is zero.")
+else:
+    print(f"{n} is a negative number.")
