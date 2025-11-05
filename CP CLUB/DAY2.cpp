343fa134720bfb7005cb8e0d8528dd5da9275334
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-05 21:38:26 +0600

    Create DAY2.cpp

---
+++ CP CLUB/DAY2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main()
+{
+
+    // advcance
+    /*
+    vector
+    it
+    push back
+    pop back
+    insert
+    */
+    /*vector<ll> arr;
+        auto it = arr.begin();
+        advanced(arr    )
+
+        for (int i = 0; i < 3; i++)
+        {
+            arr.push_back(4);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            pop_back();
+            cout << arr[i] << nl;
+        }
+        //advance()*/
+
+        ll unordered_map<ll, string> mp;
+        
+
+    return 0;
+}
