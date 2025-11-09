3ba184e7ffebf2caccc9a3a6836e7fb9b53c4b89
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-09 07:59:24 +0600

    Create sdffffff.c

---
+++ CSE14 intake contest by Moin Vai 9.2.2023/sdffffff.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,t;
+    scanf("%d",&t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d%d%d",&a,&b,&c);
+        if(a==b+c)
+        {
+            printf("YES\n");
+                return 0;
+        }
+       else if
+       {
+           printf("NO\n");
+
+       }
+
+       else if(b==a+c)
+        {
+            printf("YES\n");
+        }
+       else
+       {
+           printf("NO\n");
+       }
+
+       if(c==b+a)
+        {
+            printf("YES\n");
+        }
+       else
+       {
+           printf("NO\n");
+       }
+
+    }
+
+
+
+
+
+
+    return 0;
+}
