7eb424c7eb1eef14fa0e7e5594f5c32962fc55e7
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-09 07:59:18 +0600

    Create dddddd.c

---
+++ CSE14 intake contest by Moin Vai 9.2.2023/dddddd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+float a,a1=0,a2=0,a3=0, Case =0;
+int i,t;
+scanf("%d",&t);
+for(i=1; i<=t; i++)
+{
+scanf("%f",&a);
+float x= 2*a;
+float pi= 2*acos(0.0);
+a1=x*x;
+a2= pi*a*a;
+a3= a1-a2;
+printf("Case 1:%.2f\n",a3);
+count++;
+}
+
+return 0;
+}
