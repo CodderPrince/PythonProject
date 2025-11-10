5da99bfc84d3f1d787e6e15a95424c5f26545911
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-10 08:57:01 +0600

    Create dis.cpp

---
+++ CSES/dis.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main()
+{
+    set<ll> arr = {2, 2, 3, 2, 3};
+
+    ll total = arr.size();
+
+    cout << total << nl;
+
+    return 0;
+}
