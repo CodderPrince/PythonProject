7ccdad06df83d0e4b4117d515650d62c44b6e905
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-15 00:24:52 +0600

    Create dffds.c

---
+++ Discrete_Mathematics/dffds.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    int year=1988;
+    char code='c';
+//scanf("%d %c",&year,&code);
+//printf("year = %d\tcode = %c\n",year,code);
+//scanf("%c %d",&year, &code);
+//printf("year = %c\tcode = %d\n",year,code);
+scanf("%d %c",&code, &year);
+printf("tcode = %c\tyear = %d\n",year,code);
+//scanf("%s %c", &year, &code);
+//printf("year = %s\tcode = %c\n",year,code);
+
+
+  return 0;
+}
