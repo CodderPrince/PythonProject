d0c7197d6875b0cc231afe9c079db56eb4616994
Md. An Nahian Prince <annahian44@gmail.com>
2025-11-15 00:24:50 +0600

    Create dff.c

---
+++ Discrete_Mathematics/dff.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[] = {1,3,4,5,6,8,9,11};
+    int value=56;
+    int left =0, right =8, middle =0;
+    while(left<=right)
+    {
+        middle = (left + right)/2;
+        if(a[middle] == value)
+        {
+            printf("Item found at index: %d\n",middle);
+
+            return 0;
+        }
+        else if(a[middle]<=value)
+        {
+            left = middle+1;
+        }
+        else
+        {
+            right = middle -1;
+        }
+    }
+    printf("Item not found!\n");
+       
+}
